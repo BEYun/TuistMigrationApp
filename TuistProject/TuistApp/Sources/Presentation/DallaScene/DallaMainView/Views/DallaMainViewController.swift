a43b5f3c3f23f2ee871fd3ee71be4fa665b8f86a
BEYun <95ybya@gmail.com>
2023-04-21 08:47:17 +0900

    mainBanner와 bjStory의 view에 대한 adapter 생성

--- TuistProject/TuistApp/Sources/Presentation/DallaScene/DallaMainView/Views/DallaMainViewController.swift
+++ TuistProject/TuistApp/Sources/Presentation/DallaScene/DallaMainView/Views/DallaMainViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 import SnapKit
+import Alamofire
 
 class DallaMainViewController: UIViewController {
     
-    let dallaMainView = DallaMainView()
+    let dallaMainView: DallaMainView
+    
+    let viewModel: DallaMainViewModel
+    
+    lazy var mainBannerAdaper = MainBannerAdapter(collectionView: dallaMainView.contentView.mainBannerCollectionView)
+    lazy var bjStoryAdapter = BJStoryAdapter(collectionView: dallaMainView.contentView.bjStoryCollectionView)
 
+    init(dallaMainView: DallaMainView, viewModel: DallaMainViewModel) {
+        self.dallaMainView = dallaMainView
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func loadView() {
         self.view = dallaMainView
         view.backgroundColor = .white
@@ -20,15 +36,22 @@ class DallaMainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        mainBannerAdaper.adapterDataSource = viewModel
+        mainBannerAdaper.adapterDelegate = viewModel
+        
+        bjStoryAdapter.adapterDataSource = viewModel
+        bjStoryAdapter.adapterDelegate = viewModel
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = true
-        guard let firstChild = tabBarController?.children.first else { return }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
-            self?.tabBarController?.selectedViewController = firstChild
-        }
+//        mainBannerAdaper.makeTimer()
+//        guard let firstChild = tabBarController?.children.first else { return }myView.collectionView.delegate = myView.delegate
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+//            self?.tabBarController?.selectedViewController = firstChild
+//        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
