6a59986f9e947ab69c4267a9e88fa97785f867da
BEYun <95ybya@gmail.com>
2023-04-22 17:29:51 +0900

    UIColor Extension으로 customColor 추가

--- TuistProject/TuistApp/Sources/Utils/Extensions/UIColor+CustomColors.swift
+++ TuistProject/TuistApp/Sources/Utils/Extensions/UIColor+CustomColors.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 extension UIColor {
-    static let customGray = UIColor(red: 172 / 255, green: 172 / 255, blue: 172 / 255, alpha: 1)
-    static let customBlack = UIColor(red: 48 / 255, green: 48 / 255, blue: 48 / 255, alpha: 1)
+    // MARK: Custom Font Colors
+    
+    static let grayFontColor = UIColor(red: 172 / 255, green: 172 / 255, blue: 172 / 255, alpha: 1)
+    static let blackFontColor = UIColor(red: 32 / 255, green: 32 / 255, blue: 32 / 255, alpha: 1)
+    static let bjStoryBlackFontColor = UIColor(red: 48 / 255, green: 48 / 255, blue: 48 / 255, alpha: 1)
+    
+    // MARK: Custom Background Color
+    
+    static let grayBackgroundColor = UIColor(red: 246 / 255, green: 246 / 255, blue: 246 / 255, alpha: 1)
     static let categoryBorderColor = UIColor(red: 204 / 255, green: 204 / 255, blue: 204 / 255, alpha: 1)
+    static let bjStoryActiveBackgroundColor = UIColor(red: 255 / 255, green: 60 / 255, blue: 123 / 255, alpha: 1)
+    static let bjStoryDeactiveBackgroundColor = UIColor(red: 225 / 255, green: 225 / 255, blue: 225 / 255, alpha: 1)
 }
