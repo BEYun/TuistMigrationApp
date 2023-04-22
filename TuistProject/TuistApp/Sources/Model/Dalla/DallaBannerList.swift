d9b4373ff0240cee98fef6fb747f77de1985c5f9
BEYun <95ybya@gmail.com>
2023-04-22 17:28:39 +0900

    DallaBannerList 모델

---
+++ TuistProject/TuistApp/Sources/Model/Dalla/DallaBannerList.swift
@@ -0,0 +1,23 @@
+//
+//  DallaBannerList.swift
+//  TuistApp
+//
+//  Created by BEYun on 2023/04/20.
+//  Copyright © 2023 BEYun. All rights reserved.
+//
+
+import UIKit
+
+struct DallaBannerList {
+    let memNick: String
+    let title: String?
+    let badgeSpecial: Int
+    let backgroundImageURL: String?
+    
+    init(memNick: String, title: String, badgeSpecial: Int, backgroundImageURL: String?) {
+        self.memNick = memNick
+        self.title = title
+        self.badgeSpecial = badgeSpecial
+        self.backgroundImageURL = backgroundImageURL
+    }
+}
